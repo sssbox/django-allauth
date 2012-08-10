868ddc484b30c8d22d420f1f43aee57abafdfc68
Sam Solomon <sssbox@gmail.com>
2012-08-09 23:22:04 -0700

    [master] No signin message.

--- allauth/account/utils.py
+++ allauth/account/utils.py
@@ -97,8 +97,8 @@ def perform_login(request, user, redirect_url=None):
         user.backend = "django.contrib.auth.backends.ModelBackend"
     user_logged_in.send(sender=user.__class__, request=request, user=user)
     login(request, user)
-    messages.add_message(request, messages.SUCCESS,
-                         ugettext("Successfully signed in as %(user)s.") % { "user": user_display(user) } )
+#    messages.add_message(request, messages.SUCCESS,
+#                         ugettext("Successfully signed in as %(user)s.") % { "user": user_display(user) } )
 
     if not redirect_url:
         redirect_url = get_default_redirect(request)
